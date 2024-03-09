6d5c93da62e35934e3f4fdf82348c928eed06dc2
José Carlos de Camargo <jccamargo15@gmail.com>
2024-03-09 16:45:50 -0300

    feat: newsletter service

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NewsletterService } from '../../services/newsletter.service';
 
 @Component({
   selector: 'newsletter-form',
   standalone: true,
   imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  providers: [NewsletterService],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
 export class NewsletterFormComponent {
   newsletterForm!: FormGroup;
 
-  constructor() {
+  constructor(private service: NewsletterService) {
     this.newsletterForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -20,6 +22,13 @@ export class NewsletterFormComponent {
   }
 
   onSubmit() {
-    console.log(this.newsletterForm.value);
+    if (this.newsletterForm.valid) {
+      this.service.sendData(this.newsletterForm.value.name, this.newsletterForm.value.email)
+        .subscribe({
+          complete: () => {
+            this.newsletterForm.reset();
+          }
+        });
+    }
   }
 }
